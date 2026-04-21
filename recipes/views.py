ea439dfd944a64dd44c510c76e7237c9fe31f3c5
picker770 <arehman897@gmail.com>
2026-04-21 21:40:33 +0100

    Implement full CRUD functionality

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe
+from .forms import RecipeForm
 
 def home(request):
-    return HttpResponse("Recipe app is working")
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/home.html', {'recipes': recipes})
 
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/detail.html', {'recipe': recipe})
+
+
+def recipe_create(request):
+    form = RecipeForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'recipes/form.html', {'form': form})
+
+
+def recipe_update(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    form = RecipeForm(request.POST or None, instance=recipe)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'recipes/form.html', {'form': form})
+
+
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == "POST":
+        recipe.delete()
+        return redirect('home')
+    return render(request, 'recipes/delete.html', {'recipe': recipe})
