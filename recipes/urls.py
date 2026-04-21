a28dfcd387e488e1f3da66b48fafa51f0cd5a70d
picker770 <arehman897@gmail.com>
2026-04-21 21:41:27 +0100

    Add URL routing for CRUD pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='detail'),
+    path('create/', views.recipe_create, name='create'),
+    path('update/<int:pk>/', views.recipe_update, name='update'),
+    path('delete/<int:pk>/', views.recipe_delete, name='delete'),
 ]
