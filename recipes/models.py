f8310e9069b446d97fb4f2a929c055809f211a47
picker770 <arehman897@gmail.com>
2026-04-21 20:51:36 +0100

    Add Recipe and Category models with database migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
